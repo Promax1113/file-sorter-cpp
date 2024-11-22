29ff76e965a4e5e14d91cef5b612172bb9677048
promax1113 <67706765+Promax1113@users.noreply.github.com>
2024-11-22 18:37:40 +0100

    WIP: Major feature add.
    - Added the whole sorting system
    - Cleaned up the code
    - Removed innecessary lines that were used as debug.

--- main.cpp
+++ main.cpp
@@ -1,14 +1,19 @@
+#include <cstddef>
 #include <iostream>
 #include <string>
+#include <utility>
 #include <vector>
 #include <dirent.h>
+#include <filesystem>
 #include <sys/stat.h>
 #include <sys/types.h>
+#include <algorithm>
+#include <cctype>
 
 
 using arguments_t = std::vector<std::pair<std::string, std::string>>;
 
-std::string code2string(const int code, const std::string &action = "sorted!") {
+std::string code2string(const int code, const std::string action = "sorted!") {
     switch (code) {
         case 200:
             return "Successfully " + action;
@@ -22,12 +27,29 @@ std::string code2string(const int code, const std::string &action = "sorted!") {
     }
 }
 
+std::string getExtension(std::string string){
+    if (string[0] == '.'){
+        return "NULL";
+    }
+    std::transform(string.begin(), string.end(), string.begin(), [](unsigned char c) {
+        return std::tolower(c);
+    });
+    return string.substr(string.rfind('.')+1);
+}
+
+struct File_t{
+    std::string filename;
+    // Extension without .
+    std::string extension;
+    dirent * File_d;
+};
+
 class Arguments {
 public:
     std::string sortMode;
-    std::string filepath;
+    std::string filePath;
 
-    Arguments(const int argc, const char* argv[]) : _argc(argc), _argv(argv) {}
+    Arguments(const int argc, const char* argv[]) : _argc(argc), _argv(argv) {parse();}
 
 
     void parse() {
@@ -35,16 +57,20 @@ public:
             std::cout << "No args provided!\n";
             std::exit(1);
         }
-        sortMode = _argv[1];
-        if (_argc < 2) {
-            filepath = ".";
+
+        if (_argc <= 2){
+            filePath = ".";
+        } else {
+            filePath = _argv[2];
         }
-        //return {{"mode", sortMode}, {"filepath", filepath}};
+
+        sortMode = _argv[1];
+
+
     }
 private:
     int _argc;
     const char** _argv;
-
 };
 
 class SortFiles {
@@ -58,22 +84,56 @@ public:
 
         return S_ISREG(path.st_mode);
     }
-    static int byType(const char* filepath) {
-        std::vector<std::pair<std::string, dirent*>> dirFiles;
+    static int byType(const char * filepath, const char *execName) {
+        std::vector<struct File_t> dirFiles;
+        std::vector<std::string> extensions;
         DIR *d;
         struct dirent *entry;
-        if ((d = opendir(".")) == nullptr)
-            perror("opendir() error");
+        std::string currExt;
+        std::cout << filepath;
+        struct File_t currFile;
+        execName += 2;
+
+        if ((d = opendir(filepath)) == nullptr)
+            return 404;
         else {
             while ((entry = readdir(d)) != nullptr)
                 if (isDir(entry->d_name) != 0) {
-                    dirFiles.push_back({"file", entry});
+                    if (std::string(entry->d_name) == std::string(execName)){
+                        continue;
+                    }
+                    currFile.filename = entry->d_name;
+                    currExt = getExtension(entry->d_name);
+                    if (currExt == "NULL"){
+                        continue;
+                    }
+                    if (std::find(extensions.begin(), extensions.end(), currExt) == extensions.end()){
+                        extensions.push_back(currExt);
+                    }
+                    currFile.extension = currExt;
+                    currFile.File_d = entry;
+                    dirFiles.push_back(currFile);
                 }
             closedir(d);
         }
-        for (int i = 0; i < dirFiles.size(); i++) {
+        for (int i = 0; i < extensions.size(); i++) {
+            std::string createFilepath;
+            if (filepath == std::string(".")){
+                createFilepath = "";
+            } else {
+                createFilepath = filepath;
+            }
 
-            std::cout << i + 1<< ". " << dirFiles.at(i).second->d_name << "\n";
+            std::filesystem::path fullPath = std::filesystem::current_path() / std::filesystem::path(createFilepath) / extensions.at(i);
+
+            if (!std::filesystem::create_directory(fullPath)){
+                 std::cerr << "Failed to create directory: " << fullPath << std::endl;
+                 return 400;
+            }
+        }
+
+        for (int i = 0; i < dirFiles.size(); i++) {
+            std::filesystem::rename(std::filesystem::current_path() / dirFiles.at(i).filename, std::filesystem::current_path() / std::filesystem::path(dirFiles.at(i).extension) / dirFiles.at(i).filename);
         }
 
         return 200;
@@ -81,24 +141,22 @@ public:
 };
 
 
-
-int main(const int argc, const char * argv[]) {
+int main(const int argc, const char *argv[]) {
     Arguments args(argc, argv);
-    args.parse();
 
     if (args.sortMode == "--type" || args.sortMode == "-t") {
-        std::string result = code2string(SortFiles::byType(args.filepath.c_str()));
-        std::cout << "Status: " << result << "\n";
+        std::string result = code2string(SortFiles::byType(args.filePath.c_str(), argv[0]));
+        std::cout << result << "\n";
     }
     else if (args.sortMode == "--help" ||args.sortMode == "-h") {
-        std::cout << argv[0] << "<sort mode> <filepath>\n";
+        std::cout << argv[0] << " <sort mode> <filepath>\n";
         std::cout << "Sort mode options:\n --type, -t -> Sorts by type.\n";
-        std::cout << "--help, -h -> Shows help message.";
+        std::cout << "--help, -h -> Shows help message.\n";
     }
     else {
-        std::cout << argv[0] << "<sort mode> <filepath>\n";
+        std::cout << argv[0] << " <sort mode> <filepath>\n";
         std::cout << "Sort mode options:\n --type, -t -> Sorts by type.\n";
-        std::cout << "--help, -h -> Shows help message.";
+        std::cout << "--help, -h -> Shows help message.\n";
     }
 
     return 0;
